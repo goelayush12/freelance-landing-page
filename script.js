090ca73809d87e397f1dd81693862ddf142a4104
goelayush12 <goelayush12359@gmail.com>
2026-02-09 23:20:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("startBtn");
+const statusText = document.getElementById("status");
+
+button.addEventListener("click", () => {
+  statusText.textContent = "🚀 Journey Started! Building skills every day.";
+});
